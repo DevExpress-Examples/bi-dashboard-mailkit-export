f233d4a13e4daa7e87019a4d1cb9e510e0ab49cb
ritazakhodyaeva <57389482+ritazakhodyaeva@users.noreply.github.com>
2022-01-25 14:33:00 +0300

    Update Form1.vb

--- VB/SimpleMailExport/Form1.vb
+++ VB/SimpleMailExport/Form1.vb
@@ -16,8 +16,6 @@ Namespace SimpleMailExport
         End Sub
 
         Private Shared Function CreateMimeMessage() As MimeMessage
-            ' Instantiate a report. 
-            ' Email export options are already specified at design time.
             Try
                 Dim exporter As DashboardExporter = New DashboardExporter()
                 AddHandler exporter.ConnectionError, AddressOf Exporter_ConnectionError
@@ -54,9 +52,9 @@ Namespace SimpleMailExport
 
         Private Shared Async Function SendAsync(ByVal smtpHost As String, ByVal smtpPort As Integer, ByVal userName As String, ByVal password As String) As Task(Of String)
             Dim result As String = "OK"
-            ' Create a new memory stream and export the report in PDF.
+        ' Create a new memory stream and export the dashboard in PDF.
             Using mail As MimeMessage = CreateMimeMessage()
-                If mail Is Nothing Then Return "An error occured when export a Dashboard. See console for details."
+            If mail Is Nothing Then Return "An error occured when export a dashboard. See console for details."
                 Using client = New SmtpClient()
                     Try
                         client.Connect(smtpHost, smtpPort, SecureSocketOptions.Auto)
